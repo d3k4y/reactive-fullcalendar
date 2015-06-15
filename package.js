ced7d1865d407fe33cbe6d866fb3d081d843613a
Mike Kornelson <mike@koretech.ca>
2015-06-15 10:25:58 -0500

    Updated fullCalendar to 2.3.2 Changed moment dependency to official package

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   summary: "Meteor Reactive - Full-sized drag & drop event calendar",
-  version: "2.3.1",
-  git: "https://github.com/fermuch/meteor-reactive-fullcalendar-unstyled.git",
-  name: 'fermuch:meteor-reactive-fullcalendar'
+  version: "2.3.2",
+  git: "https://github.com/koretech/meteor-reactive-fullcalendar.git",
+  name: 'krt:meteor-reactive-fullcalendar'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0');
-  api.use('mrt:moment@2.8.1', 'client');
-  api.use('reactive-var', 'client');
+  api.use('momentjs:moment', 'client');
   api.use('templating', 'client');
   api.addFiles([
       'fullcalendar.js',
@@ -17,6 +16,6 @@ Package.onUse(function(api) {
       'gcal.js',
       'reactive-fullcalendar.js',
       'reactive-fullcalendar-template.html',
-      'reactive-fullcalendar-template.js',
+      'reactive-fullcalendar-template.js'
   ],'client');
 });
